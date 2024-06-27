94e0ba7b5aa3558519a61a2f0e1e482f3b3df2dd
Sergii <ostapec.serg@gmail.com>
2024-06-27 16:35:24 +0300

    [TPN-0006] History of comments to details and their q-ty: [ADD] translation

--- st_change_history/models/quotation_calculation_line.py
+++ st_change_history/models/quotation_calculation_line.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from odoo import fields, models
+from odoo import fields, models, api, _
 
 
 class QuotationCalculationLine(models.Model):
@@ -8,24 +8,33 @@ class QuotationCalculationLine(models.Model):
 
     comment = fields.Text(string='Comment')
 
+    @api.model
+    def create(self, vals_list):
+        result = super(QuotationCalculationLine, self).create(vals_list)
+        if 'comment' in vals_list:
+            result._add_history_record(vals_list['comment'])
+        return result
+
     def write(self, vals):
         result = super().write(vals)
-        if result and vals.get('comment', False):
-            self._add_history_record(vals.get('comment'))
+        if result and 'comment' in vals:
+            self._add_history_record(vals['comment'])
         return result
 
     def _add_history_record(self, comment: str):
         """ Add new record or update existing record. """
         history = self.env['calculation.history']
-        record = history.search([('line_id', '=', self.id)])
+        history_record = history.search([('line_id', '=', self.id)])
         record_data = {
             'author_id': self.env.user.id,
-            'comment': comment,
             'comment_date': date.today(),
         }
-        if record:
-            record.write(record_data)
-        else:
-            record_data.update({'line_id': self.id,})
-            record = history.create(record_data)
-        return record
+        if history_record:
+            comment = comment if comment else _('[Deleted]')
+            record_data.update({'comment': comment})
+            history_record.write(record_data)
+        elif comment and not history_record:
+            record_data.update({'line_id': self.id, 'comment': comment})
+            history_record = history.create(record_data)
+        if history_record:
+            return history_record
