67402ec7754ea28a8ed715fc415715f056eb1846
Sergii <ostapec.serg@gmail.com>
2024-07-02 21:21:03 +0300

    [TPN-0017] Tasks assignment: Module development

---
+++ st_calculation_activity/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Calculation Activity',
+    'version': '17.0.0.0.0',
+    'summary': 'Calculation Activity',
+    'sequence': 10,
+    'description': """
+""",
+    'category': 'Sales',
+    'website': 'https://smarttek.solutions/',
+    'depends': [
+        'st_calculation',
+    ],
+    'data': [
+        'data/activity_type_data.xml',
+        'views/quotation_calculation_views.xml'
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'st_calculation_activity/static/src/js/inherit_systray_activity_menu.js',
+        ]
+    },
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'license': 'OPL-1',
+}
