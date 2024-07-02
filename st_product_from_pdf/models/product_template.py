f800e213a63b6aca3d50b939faef33cc8a99886b
Sergii <ostapec.serg@gmail.com>
2024-07-02 18:12:46 +0300

    [TPN-0015] Generate Products from downloaded PDF files: delete _sql_constraints

--- st_product_from_pdf/models/product_template.py
+++ st_product_from_pdf/models/product_template.py
@@ -4,8 +4,4 @@ from odoo import models, fields, _
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    _sql_constraints = [
-        ('name_uniq', 'unique(name)', _('Unique product template name')),
-    ]
-
     attached_file = fields.Many2one(comodel_name='product.document', string='Attached file')
