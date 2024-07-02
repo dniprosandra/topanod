403f4c3f72812c1db78d8c4c01620cce5a7f5dee
Sergii <ostapec.serg@gmail.com>
2024-07-02 17:29:52 +0300

    [TPN-0015] Generate Products from downloaded PDF files: Code cleaning

--- st_product_from_pdf/models/product_template.py
+++ st_product_from_pdf/models/product_template.py
@@ -5,7 +5,7 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     _sql_constraints = [
-        ('name_uniq', 'UNIQUE (name)', _('Unique product template name')),
+        ('name_uniq', 'unique(name)', _('Unique product template name')),
     ]
 
     attached_file = fields.Many2one(comodel_name='product.document', string='Attached file')
